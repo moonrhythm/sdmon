10d430b2c4f2a78d5719c3e13681a741d2dd5029
Thanatat Tamtan <acoshift@gmail.com>
2020-06-09 16:38:57 +0700

    add reportf

--- report.go
+++ report.go
@@ -7,7 +7,7 @@ import (
 	"cloud.google.com/go/errorreporting"
 )
 
-// Report reports error
+// Report reports error (non-blocking)
 // request, user, stack can be empty
 func Report(err interface{}, r *http.Request, user string, stack []byte) {
 	if errorClient == nil {
@@ -27,4 +27,9 @@ func Report(err interface{}, r *http.Request, user string, stack []byte) {
 		User:  user,
 		Stack: stack,
 	})
+}
+
+// Reportf reports error (non-blocking) using fmt.Errorf
+func Reportf(format string, v ...interface{}) {
+	Report(fmt.Errorf(format, v...), nil, "", nil)
 }
